b83378ef3e6fce9f2d9799bae1b8c48038b33622
John Fuller <fuller.john84@gmail.com>
2023-12-14 18:31:52 -0500

    Finished App.

--- Unit3/Block24/Puppy Pals/src/App.jsx
+++ Unit3/Block24/Puppy Pals/src/App.jsx
@@ -1,21 +1,39 @@
-import './App.css'
 import { puppyList }  from './data.js'
 import { useState } from 'react'
+import './App.css'
+
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
-  
   console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  
+  function handleClick(puppyId){
+    setFeatPupId(puppyId);
 
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup: ", featuredPup);
   return (
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-          })
-        }
-      </div>
+    <div className='App'>
+      {puppies.map((puppy) => {
+        return (
+          <div onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+            
+          </div>
+        )
+      })}
+      {featPupId && (
+              <div>
+                <h2>{ featuredPup.name }</h2>
+                <ul>
+                  <li>Age: { featuredPup.age }</li>
+                  <li>Email: { featuredPup.email }</li>
+                </ul>
+              </div>
+            )}
+    </div>
   );
 }
 
